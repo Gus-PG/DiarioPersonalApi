fb06167204221a3e3384a1af1eaca91be8f29b40
Gustavo Pérez <perezgordo.gustavo@gmail.com>
2025-02-28 21:09:38 +0100

    Creamos DbContext y Modelo Entrada

---
+++ DiarioPersonalApi/Data/DiarioDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DiarioPersonalApi.Data
+{
+    public class DiarioDbContext : DbContext
+    {
+        public DiarioDbContext(DbContextOptions<DiarioDbContext> options) : base(options) { }
+
+        public DbSet<Entrada> Entradas { get; set; }
+    }
+}
---
+++ DiarioPersonalApi/Models/Entrada.cs
@@ -0,0 +1,9 @@
+﻿namespace DiarioPersonalApi.Models
+{
+    public class Entrada
+    {
+        public int Id {  get; set; }
+        public string Contenido { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
