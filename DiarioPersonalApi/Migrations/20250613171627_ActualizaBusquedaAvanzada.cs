08ed231a43254f0df15b16abe12530ed976b5da8
Gustavo Pérez <perezgordo.gustavo@gmail.com>
2025-06-13 19:21:03 +0200

    --> MODIFICACIONES MIGACIONES: Como habíamos modificado 'FiltroBusqeudaDTO' había que actualziar las migraciones para poder publicar. Lo hacemos a través de PowerShell con 'dotnet tool restore' y 'dotnet ef migrations add ActualizaBusquedaAvanzada'.

---
+++ DiarioPersonalApi/Migrations/20250613171627_ActualizaBusquedaAvanzada.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiarioPersonalApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class ActualizaBusquedaAvanzada : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
